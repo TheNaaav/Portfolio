fa644e238aea23c3f7d934ac3ad9cf4fe7a26885
Naruebet Singsathon <112544230+TheNaaav@users.noreply.github.com>
2024-11-07 15:13:48 +0100

    add vercel web analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import { Skills } from "./components/Skills";
 import { Contact } from "./components/Contact";
 import './index.css'
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
     <div className="bg-blue-900">
+      <Analytics />
      <Hero/>
      <div className="mt-10 ">
      <Projects/>
